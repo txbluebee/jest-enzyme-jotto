1b489e9efb1a663d949b45c8e126fad0ea605441
Brian Fan <kuomu.fan@gmail.com>
2019-06-25 14:04:54 -0700

    Set up redux

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 
-import Congrats from './Congrats'
-import GuessedWords from './GuessedWords'
+import Congrats from './Congrats';
+import GuessedWords from './GuessedWords';
 
 function App() {
   return (
-    <div className="container">
+    <div className='container'>
       <h1>Jotto</h1>
-      <Congrats success={true}/>
-      <GuessedWords guessedWords={[
-        {
-          guessedWord: 'train', letterMatchCount: 3
-        }
-      ]}/>
+      <Congrats success={true} />
+      <GuessedWords
+        guessedWords={[
+          {
+            guessedWord: 'train',
+            letterMatchCount: 3
+          }
+        ]}
+      />
     </div>
   );
 }
---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @param { arary } state - Array of guessed words.
+ * @param { object } action - action to be reduced
+ * @returns { boolean } - new sucess state
+ */
+
+export default (state, action) => {
+  return null;
+};
