11098d5ceaf5c5450f4a0a7c5097be29c3137dc3
Brian Fan <kuomu.fan@gmail.com>
2019-06-27 14:27:11 -0700

    Create tests and functionalities for  argument from input box

--- src/Input.js
+++ src/Input.js
@@ -5,13 +5,33 @@ import { guessWord } from './actions';
 
 const mapState = ({ success }) => ({ success });
 
-class Input extends Component {
+export class UnconnectedInput extends Component {
+  /**
+   * Create ref for input box
+   * @method
+   * @param  {object} props - Component props
+   * @returns {undefined}
+   */
+  constructor(props) {
+    super(props);
+    this.inputBox = React.createRef();
+  }
+
+  submitGuessedWord = e => {
+    e.preventDefault();
+    const guessedWord = this.inputBox.current.value;
+    if (guessedWord && guessedWord.length > 0) {
+      this.props.guessWord(guessedWord);
+    }
+  };
+
   renderContent = () => {
     return this.props.success ? null : (
       <form className='form-inline'>
         <input
           className='mb-2 mx-sm-3'
           id='text'
+          ref={this.inputBox}
           placeholder='enter guess'
           data-test='input-box'
         />
@@ -19,6 +39,7 @@ class Input extends Component {
           type='submit'
           className='btn btn-primary mb-2'
           data-test='submit-button'
+          onClick={this.submitGuessedWord}
         >
           Submit
         </button>
@@ -26,6 +47,10 @@ class Input extends Component {
     );
   };
 
+  /**
+   * @method render
+   * @returns {JSX.Element} - Rendered component
+   */
   render() {
     return <div data-test='component-input'>{this.renderContent()}</div>;
   }
@@ -34,4 +59,4 @@ class Input extends Component {
 export default connect(
   mapState,
   { guessWord }
-)(Input);
+)(UnconnectedInput);
